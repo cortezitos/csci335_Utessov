01d36b44763b6612c0549ae7938adae7e80c6641
Azat <utesov.azat@gmail.com>
2024-09-15 18:47:08 +0500

    new

--- alu/alu.cpp
+++ alu/alu.cpp
@@ -1,4 +1,4 @@
-#include "Vfibonacci_generator.h"
+#include "Valu.h"
 #include "verilated.h"
 #include <iostream>
 
@@ -7,12 +7,11 @@ int main(int argc, char **argv) {
     Verilated::commandArgs(argc, argv);
 
     // Instantiate the Verilog module
-    Vfibonacci_generator *tb = new Vfibonacci_generator;
+    Valu *tb = new Valu;
 
     // Simulation setup
     vluint64_t sim_time = 0;  // Simulation time
     const vluint64_t max_sim_time = 1000;  // Maximum simulation time
-    bool enable = true;  // Enable signal
 
     // Initialize the clock and reset
     tb->clk = 0;
@@ -28,16 +27,16 @@ int main(int argc, char **argv) {
             tb->rst = 0;  // Deassert reset
         }
 
-        // Apply the enable signal
-        tb->enable = enable;
-
         // Evaluate the model (tick the clock)
         tb->eval();
 
-        // Print Fibonacci number output on the rising edge of the clock
-        if (tb->clk && !tb->rst) {
-            std::cout << "Cycle " << sim_time / 2 << ": Fibonacci number = " << tb->fib_out << std::endl;
-        }
+        tb->carry_in = 0;
+        tb->in_a = 15;
+        tb->in_b = 23;
+        tb->select= 4'b1001;
+        tb->mode = 0;
+
+        std::cout << ": Fibonacci number = " << tb->alu_out << std::endl;
 
         // Increment simulation time
         sim_time++;
