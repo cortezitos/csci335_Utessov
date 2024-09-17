74c9c80457b33fa7761e2a9e39437847b9b4d0c5
Azat <utesov.azat@gmail.com>
2024-09-17 14:21:32 +0500

    new

--- alu/alu.v
+++ alu/alu.v
@@ -86,7 +86,7 @@ always @(*) begin
         4'b0010: alu_out = A | ~B;
         4'b0011: alu_out = -1;
         4'b0100: alu_out = A | (A & ~B);
-        4'b0101: {carry_out, alu_out} = {(A | B) + (A & ~B)};
+        4'b0101: alu_out = (A | B) + (A & ~B);
         4'b0110: alu_out = A - B - carry_in;
         4'b0111: alu_out = (A & ~B) - carry_in;
         4'b1000: alu_out = A + (A & B) + carry_in;
