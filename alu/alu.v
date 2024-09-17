193c422fc2e087b79086f8937a1726850d5e2e3c
Azat <utesov.azat@gmail.com>
2024-09-17 14:23:51 +0500

    new

--- alu/alu.v
+++ alu/alu.v
@@ -87,16 +87,16 @@ always @(*) begin
         4'b0011: alu_out = -1;
         4'b0100: alu_out = A | (A & ~B);
         4'b0101: alu_out = (A | B) + (A & ~B);
-        4'b0110: alu_out = A - B - carry_in;
-        4'b0111: alu_out = (A & ~B) - carry_in;
-        4'b1000: alu_out = A + (A & B) + carry_in;
-        4'b1001: alu_out = A + B + {15'b0, carry_in};
-        4'b1010: alu_out = (A | B) + (A & B) + {15'b0, carry_in};
-        4'b1011: alu_out = (A & B) - {15'b0, carry_in};
-        4'b1100: alu_out = A + A + {15'b0, carry_in};
-        4'b1101: alu_out = (A | B) + (A & ~B) + {15'b0, carry_in};
-        4'b1110: alu_out = (A | B) + (A & B) + {15'b0, carry_in};
-        4'b1111: alu_out = A - {15'b0, carry_in};
+        4'b0110: alu_out = A - B - 1;
+        4'b0111: alu_out = (A & ~B) - 1;
+        4'b1000: alu_out = A + (A & B);
+        4'b1001: alu_out = A + B;
+        4'b1010: alu_out = (A | ~B) + (A & B);
+        4'b1011: alu_out = (A & B) - 1;
+        4'b1100: alu_out = A + A;
+        4'b1101: alu_out = (A | B) + A;
+        4'b1110: alu_out = (A | ~B) + A;
+        4'b1111: alu_out = A - 1;
         default: alu_out = 16'b0;
     endcase
 end
