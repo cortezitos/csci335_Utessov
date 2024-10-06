21fbf9e1047504f078e8a9181434dc737af4ea5b
Azat <utesov.azat@gmail.com>
2024-10-06 22:22:47 +0500

    final

--- final_project/bitty/rtl/alu.v
+++ final_project/bitty/rtl/alu.v
@@ -1,30 +1,27 @@
 module alu (
-    input  wire        carry_in,
     input  wire [15:0] in_a,
     input  wire [15:0] in_b,
     input  wire [3:0]  select,
     input  wire        mode,
     output wire        carry_out,
     output wire        compare,
-    output wire [15:0]  alu_out
+    output wire [15:0] alu_out
 );
 
-    
-    wire reg [15:0] logic_out, arithmetic_out;
-    wire reg arithmetic_carry_out;
+    wire [15:0] logic_out, arithmetic_out;
+    wire arithmetic_carry_out;
 
-    logik l0 (
+    logic l (
         .select(select),
         .A(in_a),
         .B(in_b),
         .alu_out(logic_out)
     );
 
-    arithmetic a0 (
+    arithmetic a (
         .select(select),
         .A(in_a),
         .B(in_b),
-        .carry_in(carry_in),
         .carry_out(arithmetic_carry_out),
         .alu_out(arithmetic_out)
     );
@@ -33,17 +30,13 @@ module alu (
     assign carry_out = (mode == 0) ? arithmetic_carry_out : 1'b0;
     assign compare = (in_a == in_b) ? 1'b1 : 1'b0;
 
+endmodule
 
-
-
-
-
-endmodule;
-
-module logik (
+module logic (
     input wire [3:0] select, 
     input wire [15:0] A, B, 
-    output reg [15:0] alu_out);
+    output reg [15:0] alu_out
+);
 
     always @(*) begin
         case(select)
@@ -64,7 +57,6 @@ module logik (
             4'b1110: alu_out = A | B;
             4'b1111: alu_out = A;
             default: alu_out = 16'h0000;  
-            
         endcase
     end
 endmodule
@@ -72,31 +64,30 @@ endmodule
 module arithmetic (
     input wire [3:0] select,    
     input wire [15:0] A, B,            
-    input wire carry_in,        
     output reg carry_out,   
     output reg [15:0] alu_out    
 );
 
-always @(*) begin
-    case (select)
-        4'b0000: alu_out = A;
-        4'b0001: alu_out = A | B;
-        4'b0010: alu_out = A | ~B;
-        4'b0011: alu_out = -1;
-        4'b0100: alu_out = A | (A & ~B);
-        4'b0101: {carry_out, alu_out} = {1'b0, (A | B)} + {1'b0, (A & ~B)};
-        4'b0110: alu_out = A - B - 1;
-        4'b0111: alu_out = (A & ~B) - 1;
-        4'b1000: {carry_out, alu_out} = {1'b0, A} + {1'b0, (A & B)};
-        4'b1001: {carry_out, alu_out} = {1'b0, A} + {1'b0, B};
-        4'b1010: {carry_out, alu_out} = {1'b0, (A | ~B)} + {1'b0, (A & B)};
-        4'b1011: alu_out = (A & B) - 1;
-        4'b1100: {carry_out, alu_out} = {1'b0, A} + {1'b0, A};
-        4'b1101: {carry_out, alu_out} = {1'b0, (A | B)} + {1'b0, A};
-        4'b1110: {carry_out, alu_out} = {1'b0, (A | ~B)} + {1'b0, A};
-        4'b1111: alu_out = A - 1;
-        default: alu_out = 16'b0;
-    endcase
-end
-
+    always @(*) begin
+        carry_out = 0;  // Default carry_out
+        case (select)
+            4'b0000: alu_out = A;
+            4'b0001: alu_out = A | B;
+            4'b0010: alu_out = A | ~B;
+            4'b0011: alu_out = -1;
+            4'b0100: alu_out = A | (A & ~B);
+            4'b0105: {carry_out, alu_out} = {1'b0, (A | B)} + {1'b0, (A & ~B)};
+            4'b0110: alu_out = A - B - 1;
+            4'b0111: alu_out = (A & ~B) - 1;
+            4'b1000: {carry_out, alu_out} = {1'b0, A} + {1'b0, (A & B)};
+            4'b1001: {carry_out, alu_out} = {1'b0, A} + {1'b0, B};
+            4'b1010: {carry_out, alu_out} = {1'b0, (A | ~B)} + {1'b0, (A & B)};
+            4'b1011: alu_out = (A & B) - 1;
+            4'b1100: {carry_out, alu_out} = {1'b0, A} + {1'b0, A};
+            4'b1101: {carry_out, alu_out} = {1'b0, (A | B)} + {1'b0, A};
+            4'b1110: {carry_out, alu_out} = {1'b0, (A | ~B)} + {1'b0, A};
+            4'b1111: alu_out = A - 1;
+            default: alu_out = 16'b0;
+        endcase
+    end
 endmodule
