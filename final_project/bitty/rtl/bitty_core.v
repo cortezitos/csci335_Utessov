c65f1d4d4a16500fa47f932ace3f787f0b5490a9
Azat <utesov.azat@gmail.com>
2024-10-06 22:56:54 +0500

    final

--- final_project/bitty/rtl/bitty_core.v
+++ final_project/bitty/rtl/bitty_core.v
@@ -67,3 +67,214 @@ module bitty_core (
     assign done = (instruction == 16'd0); // Example done signal logic.
 
 endmodule
+
+module alu (
+    input  wire [15:0] in_a,
+    input  wire [15:0] in_b,
+    input  wire [3:0]  select,
+    input  wire        mode,
+    output wire        carry_out,
+    output wire        compare,
+    output wire [15:0] alu_out
+);
+
+    wire [15:0] logic_out, arithmetic_out;
+    wire arithmetic_carry_out;
+
+    logical l (
+        .select(select),
+        .A(in_a),
+        .B(in_b),
+        .alu_out(logic_out)
+    );
+
+    arithmetic a (
+        .select(select),
+        .A(in_a),
+        .B(in_b),
+        .carry_out(arithmetic_carry_out),
+        .alu_out(arithmetic_out)
+    );
+
+    assign alu_out = (mode == 1) ? logic_out : arithmetic_out;
+    assign carry_out = (mode == 0) ? arithmetic_carry_out : 1'b0;
+    assign compare = (in_a == in_b) ? 1'b1 : 1'b0;
+
+endmodule
+
+module logical (
+    input wire [3:0] select, 
+    input wire [15:0] A, B, 
+    output reg [15:0] alu_out
+);
+
+    always @(*) begin
+        case(select)
+            4'b0000: alu_out = ~A;
+            4'b0001: alu_out = ~(A | B);
+            4'b0010: alu_out = ~A & B;
+            4'b0011: alu_out = 16'h0000;
+            4'b0100: alu_out = ~(A & B);
+            4'b0101: alu_out = ~B;
+            4'b0110: alu_out = A ^ B;
+            4'b0111: alu_out = A & ~B;
+            4'b1000: alu_out = ~A | B;
+            4'b1001: alu_out = ~(A ^ B);
+            4'b1010: alu_out = B;
+            4'b1011: alu_out = A & B;
+            4'b1100: alu_out = 16'hFFFF; 
+            4'b1101: alu_out = A | ~B;
+            4'b1110: alu_out = A | B;
+            4'b1111: alu_out = A;
+            default: alu_out = 16'h0000;  
+        endcase
+    end
+endmodule
+
+module arithmetic (
+    input wire [3:0] select,    
+    input wire [15:0] A, B,            
+    output reg carry_out,   
+    output reg [15:0] alu_out    
+);
+
+    always @(*) begin
+        carry_out = 0;  // Default carry_out
+        case (select)
+            4'b0000: alu_out = A;
+            4'b0001: alu_out = A | B;
+            4'b0010: alu_out = A | ~B;
+            4'b0011: alu_out = -1;
+            4'b0100: alu_out = A | (A & ~B);
+            4'b0101: {carry_out, alu_out} = {1'b0, (A | B)} + {1'b0, (A & ~B)};
+            4'b0110: alu_out = A - B - 1;
+            4'b0111: alu_out = (A & ~B) - 1;
+            4'b1000: {carry_out, alu_out} = {1'b0, A} + {1'b0, (A & B)};
+            4'b1001: {carry_out, alu_out} = {1'b0, A} + {1'b0, B};
+            4'b1010: {carry_out, alu_out} = {1'b0, (A | ~B)} + {1'b0, (A & B)};
+            4'b1011: alu_out = (A & B) - 1;
+            4'b1100: {carry_out, alu_out} = {1'b0, A} + {1'b0, A};
+            4'b1101: {carry_out, alu_out} = {1'b0, (A | B)} + {1'b0, A};
+            4'b1110: {carry_out, alu_out} = {1'b0, (A | ~B)} + {1'b0, A};
+            4'b1111: alu_out = A - 1;
+            default: alu_out = 16'b0;
+        endcase
+    end
+endmodule
+
+module control_unit (
+    input  wire [15:0] instruction,
+    input  wire        run,
+    input  wire        clk,
+    input  wire        reset,
+    output reg  [3:0]  sel,
+    output reg  [2:0]  mux_sel,
+    output reg         mode,
+    output reg         en_s,
+    output reg         en_c,
+    output reg         en_0,
+    output reg         en_1,
+    output reg         en_2,
+    output reg         en_3,
+    output reg         en_4,
+    output reg         en_5,
+    output reg         en_6,
+    output reg         en_7
+);
+
+    reg [1:0] i = 0;
+
+    always @(posedge clk or posedge reset) begin
+        if (reset) begin
+            i <= 0;
+            en_s <= 0;
+            en_c <= 0;
+            en_0 <= 0;
+            en_1 <= 0;
+            en_2 <= 0;
+            en_3 <= 0;
+            en_4 <= 0;
+            en_5 <= 0;
+            en_6 <= 0;
+            en_7 <= 0;
+        end else if (run) begin
+            case(i)
+                2'd0: begin
+                    mux_sel <= instruction[15:13];
+                    en_s <= 1;
+                    i <= 1;
+                end
+                2'd1: begin
+                    mux_sel <= instruction[12:10];
+                    en_c <= 1;
+                    sel <= instruction[6:3];
+                    mode <= instruction[2];
+                    i <= 2;
+                end
+                2'd2: begin
+                    case(instruction[15:13])
+                        3'd0: en_0 <= 1;
+                        3'd1: en_1 <= 1;
+                        3'd2: en_2 <= 1;
+                        3'd3: en_3 <= 1;
+                        3'd4: en_4 <= 1;
+                        3'd5: en_5 <= 1;
+                        3'd6: en_6 <= 1;
+                        3'd7: en_7 <= 1;
+                    endcase
+                    i <= 0;
+                end
+            endcase
+        end
+    end
+
+endmodule
+
+module multiplexer (
+    input  wire [15:0] d_in,
+    input  wire [15:0] reg_0,
+    input  wire [15:0] reg_1,
+    input  wire [15:0] reg_2,
+    input  wire [15:0] reg_3,
+    input  wire [15:0] reg_4,
+    input  wire [15:0] reg_5,
+    input  wire [15:0] reg_6,
+    input  wire [15:0] reg_7,
+    input  wire  [3:0] mux_sel,
+    output reg  [15:0] d_out
+);
+
+    always @(*) begin
+        case (mux_sel)
+            4'd0: d_out = d_in;
+            4'd1: d_out = reg_0;
+            4'd2: d_out = reg_1;
+            4'd3: d_out = reg_2;
+            4'd4: d_out = reg_3;
+            4'd5: d_out = reg_4;
+            4'd6: d_out = reg_5;
+            4'd7: d_out = reg_6;
+            4'd8: d_out = reg_7;
+            default: d_out = 16'd0;
+        endcase
+    end
+endmodule
+
+module register (
+    input  wire [15:0] d_in,
+    input  wire        reset,
+    input  wire        clk,
+    input  wire        en,
+    output reg  [15:0] d_out
+);
+
+    always @(posedge clk) begin
+        if (reset) begin
+            d_out <= 16'd0;
+        end
+        else if (en) begin
+            d_out <= d_in;
+        end
+    end
+
+endmodule
