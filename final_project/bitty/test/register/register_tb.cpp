57845b0c479f87536e99a4230b3a52641d2e2fb3
Azat <utesov.azat@gmail.com>
2024-10-08 15:11:14 +0500

    final

--- final_project/bitty/test/register/register_tb.cpp
+++ final_project/bitty/test/register/register_tb.cpp
@@ -8,16 +8,21 @@ void toggle_clock(Vregister *tb) {
 }
 
 void register_load(Vregister *tb, uint16_t data_in, bool enable, bool reset) {
+        toggle_clock(tb);
+
     tb->d_in = data_in;
     tb->en = enable;
     tb->reset = reset;
     toggle_clock(tb);
+
 }
 
 int main(int argc, char **argv) {
     Verilated::commandArgs(argc, argv);
 
     Vregister *tb = new Vregister;
+
+    tb->clk = 0;
 
     std::cout << "Test 1: Reset the register\n";
     register_load(tb, 0, false, true);
