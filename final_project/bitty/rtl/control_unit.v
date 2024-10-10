f9b0730cb27467807450c563e9fa50530c609cc7
Azat <utesov.azat@gmail.com>
2024-10-10 14:33:28 +0500

    lab11

--- final_project/bitty/rtl/control_unit.v
+++ final_project/bitty/rtl/control_unit.v
@@ -3,20 +3,20 @@ module control_unit (
     input  wire        run,
     input  wire        clk,
     input  wire        reset,
-    output wire  [3:0]  sel,
-    output wire  [2:0]  mux_sel,
-    output wire         mode,
-    output wire         en_s,
-    output wire         en_c,
-    output wire         en_0,
-    output wire         en_1,
-    output wire         en_2,
-    output wire         en_3,
-    output wire         en_4,
-    output wire         en_5,
-    output wire         en_6,
-    output wire         en_7,
-    output wire        done
+    output reg  [3:0]  sel,
+    output reg  [2:0]  mux_sel,
+    output reg         mode,
+    output reg         en_s,
+    output reg         en_c,
+    output reg         en_0,
+    output reg         en_1,
+    output reg         en_2,
+    output reg         en_3,
+    output reg         en_4,
+    output reg         en_5,
+    output reg         en_6,
+    output reg         en_7,
+    output reg        done
 );
 
     reg [1:0] i = 0;
@@ -50,19 +50,19 @@ module control_unit (
                 end
                 2'd2: begin
                     case(instruction[15:13])
-                        3'd0: en_0 = 1;
-                        3'd1: en_1 = 1;
-                        3'd2: en_2 = 1;
-                        3'd3: en_3 = 1;
-                        3'd4: en_4 = 1;
-                        3'd5: en_5 = 1;
-                        3'd6: en_6 = 1;
-                        3'd7: en_7 = 1;
+                        3'd0: en_0 <= 1;
+                        3'd1: en_1 <= 1;
+                        3'd2: en_2 <= 1;
+                        3'd3: en_3 <= 1;
+                        3'd4: en_4 <= 1;
+                        3'd5: en_5 <= 1;
+                        3'd6: en_6 <= 1;
+                        3'd7: en_7 <= 1;
                     endcase
                     i <= 0;
-                    done = 1;
+                    done <= 1;
+                2'd3: i = 0;
                 end
-                default: i <= 0;
             endcase
         end
     end
