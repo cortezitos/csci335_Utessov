1f5662b2390258388e6ce9e70cb70952bcefb351
Azat <utesov.azat@gmail.com>
2024-10-15 14:18:27 +0500

    lab11

--- final_project/bitty/rtl/bitty_core.v
+++ final_project/bitty/rtl/bitty_core.v
@@ -151,29 +151,61 @@ module alu (
     output wire [15:0] alu_out
 );
 
-    wire [15:0] logic_out, arithmetic_out;
-    wire arithmetic_carry_out;
 
-    logical l (
-        .select(select),
-        .A(in_a),
-        .B(in_b),
-        .alu_out(logic_out)
-    );
+    always @(*) begin
+        case(mode)
+            1'b0: 
+            begin
+                carry_out = 0;  
+                case (select)
+                    4'b0000: alu_out = in_a;
+                    4'b0001: alu_out = in_a | in_b;
+                    4'b0010: alu_out = in_a | ~in_b;
+                    4'b0011: alu_out = -1;
+                    4'b0100: alu_out = in_a | (in_a & ~in_b);
+                    4'b0101: {carry_out, alu_out} = {1'b0, (in_a | in_b)} + {1'b0, (in_a & ~in_b)};
+                    4'b0110: alu_out = in_a - in_b - 1;
+                    4'b0111: alu_out = (in_a & ~in_b) - 1;
+                    4'b1000: {carry_out, alu_out} = {1'b0, in_a} + {1'b0, (in_a & in_b)};
+                    4'b1001: {carry_out, alu_out} = {1'b0, in_a} + {1'b0, in_b};
+                    4'b1010: {carry_out, alu_out} = {1'b0, (in_a | ~in_b)} + {1'b0, (in_a & in_b)};
+                    4'b1011: alu_out = (in_a & in_b) - 1;
+                    4'b1100: {carry_out, alu_out} = {1'b0, in_a} + {1'b0, in_a};
+                    4'b1101: {carry_out, alu_out} = {1'b0, (in_a | in_b)} + {1'b0, in_a};
+                    4'b1110: {carry_out, alu_out} = {1'b0, (in_a | ~in_b)} + {1'b0, in_a};
+                    4'b1111: alu_out = in_a - 1;
+                    default: alu_out = 16'b0;
+                endcase
+            end
 
-    arithmetic a (
-        .select(select),
-        .A(in_a),
-        .B(in_b),
-        .carry_out(arithmetic_carry_out),
-        .alu_out(arithmetic_out)
-    );
-
-    assign alu_out = (mode == 1) ? logic_out : arithmetic_out;
-    assign carry_out = (mode == 0) ? arithmetic_carry_out : 1'b0;
-    assign compare = (in_a == in_b) ? 1'b1 : 1'b0;
+            1'b1: 
+            begin
+                case(select)
+                    4'b0000: alu_out = ~in_a;
+                    4'b0001: alu_out = ~(in_a | in_b);
+                    4'b0010: alu_out = ~in_a & in_b;
+                    4'b0011: alu_out = 16'h0000;
+                    4'b0100: alu_out = ~(in_a & in_b);
+                    4'b0101: alu_out = ~in_b;
+                    4'b0110: alu_out = in_a ^ in_b;
+                    4'b0111: alu_out = in_a & ~in_b;
+                    4'b1000: alu_out = ~in_a | in_b;
+                    4'b1001: alu_out = ~(in_a ^ in_b);
+                    4'b1010: alu_out = in_b;
+                    4'b1011: alu_out = in_a & in_b;
+                    4'b1100: alu_out = 16'hFFFF; 
+                    4'b1101: alu_out = in_a | ~in_b;
+                    4'b1110: alu_out = in_a | in_b;
+                    4'b1111: alu_out = in_a;
+                    default: alu_out = 16'h0000; 
+                endcase
+            end
+        endcase
+        compare = (in_a == in_b) ? 1 : 0;
+    end
 
 endmodule
+
 
 module logical (
     input wire [3:0] select, 
