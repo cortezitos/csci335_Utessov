74b1b6b3a4cd12a8dcece7d08bce1f80c953d028
Azat <utesov.azat@gmail.com>
2024-10-15 14:36:17 +0500

    lab11

--- final_project/bitty/rtl/bitty_core.v
+++ final_project/bitty/rtl/bitty_core.v
@@ -206,68 +206,6 @@ module alu (
 
 endmodule
 
-
-module logical (
-    input wire [3:0] select, 
-    input wire [15:0] A, B, 
-    output reg [15:0] alu_out
-);
-
-    always @(*) begin
-        case(select)
-            4'b0000: alu_out = ~A;
-            4'b0001: alu_out = ~(A | B);
-            4'b0010: alu_out = ~A & B;
-            4'b0011: alu_out = 16'h0000;
-            4'b0100: alu_out = ~(A & B);
-            4'b0101: alu_out = ~B;
-            4'b0110: alu_out = A ^ B;
-            4'b0111: alu_out = A & ~B;
-            4'b1000: alu_out = ~A | B;
-            4'b1001: alu_out = ~(A ^ B);
-            4'b1010: alu_out = B;
-            4'b1011: alu_out = A & B;
-            4'b1100: alu_out = 16'hFFFF; 
-            4'b1101: alu_out = A | ~B;
-            4'b1110: alu_out = A | B;
-            4'b1111: alu_out = A;
-            default: alu_out = 16'h0000;  
-        endcase
-    end
-endmodule
-
-module arithmetic (
-    input wire [3:0] select,    
-    input wire [15:0] A, B,            
-    output reg carry_out,   
-    output reg [15:0] alu_out    
-);
-
-    always @(*) begin
-        carry_out = 0;  // Default carry_out
-        case (select)
-            4'b0000: alu_out = A;
-            4'b0001: alu_out = A | B;
-            4'b0010: alu_out = A | ~B;
-            4'b0011: alu_out = -1;
-            4'b0100: alu_out = A | (A & ~B);
-            4'b0101: {carry_out, alu_out} = {1'b0, (A | B)} + {1'b0, (A & ~B)};
-            4'b0110: alu_out = A - B - 1;
-            4'b0111: alu_out = (A & ~B) - 1;
-            4'b1000: {carry_out, alu_out} = {1'b0, A} + {1'b0, (A & B)};
-            4'b1001: {carry_out, alu_out} = {1'b0, A} + {1'b0, B};
-            4'b1010: {carry_out, alu_out} = {1'b0, (A | ~B)} + {1'b0, (A & B)};
-            4'b1011: alu_out = (A & B) - 1;
-            4'b1100: {carry_out, alu_out} = {1'b0, A} + {1'b0, A};
-            4'b1101: {carry_out, alu_out} = {1'b0, (A | B)} + {1'b0, A};
-            4'b1110: {carry_out, alu_out} = {1'b0, (A | ~B)} + {1'b0, A};
-            4'b1111: alu_out = A - 1;
-            default: alu_out = 16'b0;
-        endcase
-    end
-endmodule
-
-
 module multiplexer (
     input  wire [15:0] d_in,
     input  wire [15:0] reg_0,
