4583d3f5ac611639d7c02164afea93d6e16f1fcd
Azat <utesov.azat@gmail.com>
2024-10-15 14:03:03 +0500

    lab11

--- final_project/bitty/rtl/bitty_core.v
+++ final_project/bitty/rtl/bitty_core.v
@@ -13,36 +13,6 @@ module bitty_core (
 
     wire [15:0] reg_0_out, reg_1_out, reg_2_out, reg_3_out, reg_4_out, reg_5_out, reg_6_out, reg_7_out;
 
-    register reg_0 (.d_in(alu_out), .reset(reset), .clk(clk), .en(en_0), .d_out(reg_0_out));
-    register reg_1 (.d_in(alu_out), .reset(reset), .clk(clk), .en(en_1), .d_out(reg_1_out));
-    register reg_2 (.d_in(alu_out), .reset(reset), .clk(clk), .en(en_2), .d_out(reg_2_out));
-    register reg_3 (.d_in(alu_out), .reset(reset), .clk(clk), .en(en_3), .d_out(reg_3_out));
-    register reg_4 (.d_in(alu_out), .reset(reset), .clk(clk), .en(en_4), .d_out(reg_4_out));
-    register reg_5 (.d_in(alu_out), .reset(reset), .clk(clk), .en(en_5), .d_out(reg_5_out));
-    register reg_6 (.d_in(alu_out), .reset(reset), .clk(clk), .en(en_6), .d_out(reg_6_out));
-    register reg_7 (.d_in(alu_out), .reset(reset), .clk(clk), .en(en_7), .d_out(reg_7_out));
-
-    register reg_s (.d_in(mux_out), .reset(reset), .clk(clk), .en(en_s), .d_out(reg_s_out));
-    register reg_c (.d_in(alu_out), .reset(reset), .clk(clk), .en(en_c), .d_out(reg_c_out));
-
-    multiplexer mux (
-        .d_in(reg_s_out),
-        .reg_0(reg_0_out), .reg_1(reg_1_out), .reg_2(reg_2_out), .reg_3(reg_3_out),
-        .reg_4(reg_4_out), .reg_5(reg_5_out), .reg_6(reg_6_out), .reg_7(reg_7_out),
-        .mux_sel(mux_sel),
-        .d_out(mux_out)
-    );
-
-    alu alu (
-        .in_a(reg_s_out),
-        .in_b(mux_out),
-        .select(sel),
-        .mode(mode),
-        .carry_out(carry_out),
-        .compare(compare),
-        .alu_out(alu_out)
-    );
-
     control_unit ctrl_unit (
         .instruction(instruction),
         .run(run),
@@ -60,12 +30,116 @@ module bitty_core (
         .en_4(en_4),
         .en_5(en_5),
         .en_6(en_6),
-        .en_7(en_7)
+        .en_7(en_7),
+        .done(done)
     );
 
-    assign done = (instruction == 16'd0); // Example done signal logic.
+
+    register reg_0 (.d_in(reg_c_out), .reset(reset), .clk(clk), .en(en_0), .d_out(reg_0_out));
+    register reg_1 (.d_in(reg_c_out), .reset(reset), .clk(clk), .en(en_1), .d_out(reg_1_out));
+    register reg_2 (.d_in(reg_c_out), .reset(reset), .clk(clk), .en(en_2), .d_out(reg_2_out));
+    register reg_3 (.d_in(reg_c_out), .reset(reset), .clk(clk), .en(en_3), .d_out(reg_3_out));
+    register reg_4 (.d_in(reg_c_out), .reset(reset), .clk(clk), .en(en_4), .d_out(reg_4_out));
+    register reg_5 (.d_in(reg_c_out), .reset(reset), .clk(clk), .en(en_5), .d_out(reg_5_out));
+    register reg_6 (.d_in(reg_c_out), .reset(reset), .clk(clk), .en(en_6), .d_out(reg_6_out));
+    register reg_7 (.d_in(reg_c_out), .reset(reset), .clk(clk), .en(en_7), .d_out(reg_7_out));
+
+    register reg_s (.d_in(mux_out), .reset(reset), .clk(clk), .en(en_s), .d_out(reg_s_out));
+    register reg_c (.d_in(alu_out), .reset(reset), .clk(clk), .en(en_c), .d_out(reg_c_out));
+
+
+    multiplexer mux (
+        .d_in(reg_c_out),
+        .reg_0(reg_0_out), .reg_1(reg_1_out), .reg_2(reg_2_out), .reg_3(reg_3_out),
+        .reg_4(reg_4_out), .reg_5(reg_5_out), .reg_6(reg_6_out), .reg_7(reg_7_out),
+        .mux_sel(mux_sel),
+        .d_out(mux_out)
+    );
+
+    alu alu (
+        .in_a(reg_s_out),
+        .in_b(mux_out),
+        .select(sel),
+        .mode(mode),
+        .carry_out(carry_out),
+        .compare(compare),
+        .alu_out(alu_out)
+    );
+endmodule
+
+
+module control_unit (
+    input  wire [15:0] instruction,
+    input  wire        run,
+    input  wire        clk,
+    input  wire        reset,
+    output reg  [3:0]  sel,
+    output reg  [2:0]  mux_sel,
+    output reg         mode,
+    output reg         en_s,
+    output reg         en_c,
+    output reg         en_0,
+    output reg         en_1,
+    output reg         en_2,
+    output reg         en_3,
+    output reg         en_4,
+    output reg         en_5,
+    output reg         en_6,
+    output reg         en_7,
+    output reg        done
+);
+
+    reg [1:0] i = 0;
+
+    always @(posedge clk or posedge reset) begin
+        if (reset) begin
+            i <= 0;
+            en_s <= 0;
+            en_c <= 0;
+            en_0 <= 0;
+            en_1 <= 0;
+            en_2 <= 0;
+            en_3 <= 0;
+            en_4 <= 0;
+            en_5 <= 0;
+            en_6 <= 0;
+            en_7 <= 0;
+        end else if (run) begin
+            case(i)
+                2'd0: begin
+                    mux_sel <= instruction[15:13];
+                    en_s <= 1;
+                    i <= 1;
+                end
+                2'd1: begin
+                    mux_sel <= instruction[12:10];
+                    en_c <= 1;
+                    sel <= instruction[6:3];
+                    mode <= instruction[2];
+                    i <= 2;
+                end
+                2'd2: begin
+                    case(instruction[15:13])
+                        3'd0: en_0 <= 1;
+                        3'd1: en_1 <= 1;
+                        3'd2: en_2 <= 1;
+                        3'd3: en_3 <= 1;
+                        3'd4: en_4 <= 1;
+                        3'd5: en_5 <= 1;
+                        3'd6: en_6 <= 1;
+                        3'd7: en_7 <= 1;
+                    endcase
+                    i <= 0;
+                    done <= 1;
+                end
+                2'd3: i <= 0;
+
+            endcase
+        end
+    end
 
 endmodule
+
 
 module alu (
     input  wire [15:0] in_a,
@@ -161,73 +235,6 @@ module arithmetic (
     end
 endmodule
 
-module control_unit (
-    input  wire [15:0] instruction,
-    input  wire        run,
-    input  wire        clk,
-    input  wire        reset,
-    output reg  [3:0]  sel,
-    output reg  [2:0]  mux_sel,
-    output reg         mode,
-    output reg         en_s,
-    output reg         en_c,
-    output reg         en_0,
-    output reg         en_1,
-    output reg         en_2,
-    output reg         en_3,
-    output reg         en_4,
-    output reg         en_5,
-    output reg         en_6,
-    output reg         en_7
-);
-
-    reg [1:0] i = 0;
-
-    always @(posedge clk or posedge reset) begin
-        if (reset) begin
-            i <= 0;
-            en_s <= 0;
-            en_c <= 0;
-            en_0 <= 0;
-            en_1 <= 0;
-            en_2 <= 0;
-            en_3 <= 0;
-            en_4 <= 0;
-            en_5 <= 0;
-            en_6 <= 0;
-            en_7 <= 0;
-        end else if (run) begin
-            case(i)
-                2'd0: begin
-                    mux_sel <= instruction[15:13];
-                    en_s <= 1;
-                    i <= 1;
-                end
-                2'd1: begin
-                    mux_sel <= instruction[12:10];
-                    en_c <= 1;
-                    sel <= instruction[6:3];
-                    mode <= instruction[2];
-                    i <= 2;
-                end
-                2'd2: begin
-                    case(instruction[15:13])
-                        3'd0: en_0 <= 1;
-                        3'd1: en_1 <= 1;
-                        3'd2: en_2 <= 1;
-                        3'd3: en_3 <= 1;
-                        3'd4: en_4 <= 1;
-                        3'd5: en_5 <= 1;
-                        3'd6: en_6 <= 1;
-                        3'd7: en_7 <= 1;
-                    endcase
-                    i <= 0;
-                end
-            endcase
-        end
-    end
-
-endmodule
 
 module multiplexer (
     input  wire [15:0] d_in,
@@ -259,6 +266,7 @@ module multiplexer (
     end
 endmodule
 
+
 module register (
     input  wire [15:0] d_in,
     input  wire        reset,
@@ -277,3 +285,6 @@ module register (
     end
 
 endmodule
+
+
+
---
+++ final_project/bitty/test/bitty_core/bitty_core_tb.cpp
@@ -0,0 +1,48 @@
+#include "Vbitty_core.h"
+#include "verilated.h"
+#include <iostream>
+
+void toggle_clock(Vbitty_core *tb) {
+    tb->clk = !tb->clk;
+    tb->eval();
+}
+
+
+
+int main(int argc, char **argv) {
+    Verilated::commandArgs(argc, argv);
+
+    Vbitty_core *tb = new Vbitty_core;
+
+    tb->clk = 0;
+    tb->instruction = 0b0010100001100100;
+    tb->reset = 0;
+    tb->run = 1;
+
+    toggle_clock(tb);
+    toggle_clock(tb);
+    toggle_clock(tb);
+
+    std::cout << "Test 1: Instruction i = 0\n";
+    std::cout << "mux_sel = " << static_cast<int>(tb->mux_sel) << " en_s = " << static_cast<int>(tb->en_s) << " (Expected: 2 1)\n";
+
+    toggle_clock(tb);
+    toggle_clock(tb);
+    toggle_clock(tb);
+
+
+    std::cout << "Test 2: Instruction i = 1\n";
+    std::cout << "mux_sel = " << static_cast<int>(tb->reg_c_out) << "\n";
+
+    toggle_clock(tb);
+    toggle_clock(tb);  
+    toggle_clock(tb);
+
+    std::cout << "Test 3: Instruction i = 2\n";
+    std::cout << "en_2 = " << static_cast<int>(tb->reg_0_out);
+
+    tb->final();
+    delete tb;
+
+    return 0;
+}
