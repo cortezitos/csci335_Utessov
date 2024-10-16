28ff1e923c379b1e42bb93675cfc75d6bd44ba00
Azat <utesov.azat@gmail.com>
2024-10-16 16:24:47 +0500

    lab11

--- final_project/bitty/rtl/multiplexer.v
+++ final_project/bitty/rtl/multiplexer.v
@@ -13,14 +13,14 @@ module multiplexer (
 
     always @(*) begin
         case (mux_sel)
-            4'd0: d_out = reg_0;
-            4'd1: d_out = reg_1;
-            4'd2: d_out = reg_2;
-            4'd3: d_out = reg_3;
-            4'd4: d_out = reg_4;
-            4'd5: d_out = reg_5;
-            4'd6: d_out = reg_6;
-            4'd7: d_out = reg_7;
+            3'd0: d_out = reg_0;
+            3'd1: d_out = reg_1;
+            3'd2: d_out = reg_2;
+            3'd3: d_out = reg_3;
+            3'd4: d_out = reg_4;
+            3'd5: d_out = reg_5;
+            3'd6: d_out = reg_6;
+            3'd7: d_out = reg_7;
             default: d_out = 16'd0;
         endcase
     end
